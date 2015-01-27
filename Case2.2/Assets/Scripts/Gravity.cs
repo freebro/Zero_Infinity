dd0c597f23244cbfd5a8d2ccbd6a012ec999dd6c
freebro <tijl.cools@telenet.be>
2015-01-27 14:13:23 +0100

    adding of gravity and movement

---
+++ Case2.2/Assets/Scripts/Gravity.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+	GameObject World;
+	Vector3 Distance;
+	float gravity=9.81f;
+
+	void Start () 
+	{
+		World = GameObject.FindGameObjectWithTag ("World");
+	}
+
+	void Update () 
+	{
+		fGravity ();
+		fForce ();
+
+	}
+
+	void fGravity()
+	{
+		Distance = World.transform.position - transform.position;
+	}
+
+	void fForce()
+	{
+		rigidbody.AddForce (Distance*gravity);
+	}
+}
---
+++ Case2.2/Assets/Scripts/PController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PController : MonoBehaviour {
+	int speed = 4000;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetAxis ("Horizontal") !=0)
+		{
+			Debug.Log ("Hor");
+			rigidbody.AddRelativeForce (new Vector3(Input.GetAxis ("Horizontal")*Time.deltaTime*speed,0,0));
+		}
+		if(Input.GetAxis ("Vertical")!=0)
+		{
+			Debug.Log("ver");
+			rigidbody.AddRelativeForce (new Vector3(0,Input.GetAxis ("Vertical")*Time.deltaTime*speed,0));
+		}
+	}
+}
